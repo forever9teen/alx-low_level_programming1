d662ad812cf3a6f73c22407c9bb9f29f4240dcef
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-07 06:42:57 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-echo "Programming is like building a multilingual puzzle/n"
+#include <stdio.h>
+/**
+ * * * main. Entry point
+ * * *
+ * * *Return: Always 0 (success)
+ *  **/
+int main()
+{
+	puts("Programming is like building a multilingual puzzle/n");
+	 
+	           return (0);
+
+}
