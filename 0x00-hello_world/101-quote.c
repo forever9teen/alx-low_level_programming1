8a00a23ab1bfebf9de0466ec3512816fd6553a7e
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-08 07:52:11 -0700

    8.UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 /**
  * * * main - Entry Point
  * * * - prints and that piece of art is useful - Dora Korpar, 2015-10-19
- * * *Retrun; Always 1 (success)
+ * * *Retrun: Always 1 (success)
  *    **/
 int main(void)
 {
 	write("and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
