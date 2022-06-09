600d6004c378e52c99f97738adb62a7b776d7ff9
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-09 15:17:14 -0700

    6. Size is not grandeur, and territory does not make a nation
    mandatory

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * * * main - Entry Point
- * * * - prints and that piece of art is useful - Dora Korpar, 2015-10-19
+ * * * prints and that piece of art is useful - Dora Korpar, 2015-10-19
  * * *Retrun: Always 1 (success)
  *    **/
 int main(void)
 {
-	write("and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+	  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\m", 59);
+	  return (1);
 }
