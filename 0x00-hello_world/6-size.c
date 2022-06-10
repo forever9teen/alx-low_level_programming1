763e1c232bbedbf8ca4bfb8ff5a9adfa83ab5ff7
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-09 17:18:00 -0700

    6.Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *  * * Main - Entry Point 
+ *  * * Main-Entry Point 
  *  * * Prints out the data types
  *  *  * Return: Always 0 (success)
  *    **/
 int main(void)
-{
-	int a;
+{       int a;
 	long int b;
 	long long int c;
 	char d;
