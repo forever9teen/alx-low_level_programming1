02d051d06087b1a8cbdd7bae1488813acff69668
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-10 16:29:30 -0700

    1.LAST

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 		printf("and is greater than 5 \n");
 	if (n == 0)
 		printf("and is 0\n");
-	if (n < 6 && n ! = 0)
+	if (n < 6 && n != 0)
 		printf("and is less than 6 and not 0\n");
 	return (0);
 }
