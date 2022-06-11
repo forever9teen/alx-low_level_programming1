39c186155f16a3a0a905f629cdec5cddfb1ef5b1
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-11 09:14:08 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  * * main - prints all single digit numbers
+ *  * base 10 starting from 0, followed by a new line.
+ *  * *Return: ALways 0 (success)
+ *  **/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("d", n);
+	printf9("\n");
+	return (0);
+}
