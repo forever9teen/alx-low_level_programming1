676de90434de7cb8b24de1e67c06da3b0f9aa2f0
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-13 05:43:26 -0700

    .11

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,19 +8,24 @@ int main(void)
 {
 	int ch;
 	int n;
+	int m;
 
 	for (ch = 48; ch <= 57; ch++)
 	{
-		for (n =48; n<= 57; n++)
+		for (n =49; n <= 57; n++)
 		{
-			if (n > ch)
+			for (m = 50; m <= 57; m++)
 			{
-				putchar(ch);
-				putchar(n);
-				if (ch != 56 || n != 57)
+				if (n > ch && m > n)
 				{
-					putchar(44);
-					putchar(32);
+					putchar(ch);
+					putchar(n);
+					putchar(m);
+					if (ch != 55 || n != 56 || m != 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
 				}
 			}
 		}
