90262296cc518bb36d067cdf9a90231538277d11
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-15 15:43:19 -0700

    .6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,11 @@
 /**
  * * *int _abs - return absolute value of a number
  * @n: Parameter
- * *Return: Absolute value of an integer
+ * *Return: Absolute value of a number
  * **/
 int _abs(int n)
 {
 	if (n < 0)
-		n = -n;
+		n = -n/;
 	return (n);
 }
