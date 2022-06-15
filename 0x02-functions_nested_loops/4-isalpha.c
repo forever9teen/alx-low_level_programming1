9ebc6b4e1fbf02f7763c3a7a4def47d24ead27a9
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-15 01:31:34 -0700

    .4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * * *  _isalpha -  a function that checks for alphabetic character.
+ * * *_isalpha - a function that checks for alphabetic character.
  * * @c: a character of lowercase or uppercase
- * * *Return: returns 1 if c is a letter, lowercase or uppercase. returns 0 otherwise
+ * * *Return: returns 1 if c is a letter, lowercase or uppercase.
  *  **/
 int _isalpha(int c)
 {
