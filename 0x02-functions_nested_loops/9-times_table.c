a1c965021595cbbacb3233ef88cd1d393b25d402
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-15 19:35:16 -0700

    .9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * * * times_table - prints the 9 times table
+ * starting with 0.
+ * **/
+void times_table(void)
+{
+	int y;
+	int b;
+	int c;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = y * b;
+			if (b == 0)
+				putchar(c + '0');
+			if (b != 0 && c < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+		} 
+		_putchar('\n');
+	}
+}
