7620cc310ed363cd1dcb0b60a414e4c5f1e7c50a
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-15 19:18:43 -0700

    .8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * * * jack_bauer-  prints every minute of the day of Jack Bauer
+ * * * jack_bauer - prints every minute of the day of Jack Bauer
  * *  starting from 00:00 to 23:59.
  * **/
 void jack_bauer(void)
@@ -30,7 +30,7 @@ void jack_bauer(void)
 			}
 			else if (j >= 10)
 			{
-				putchar(':');
+				_putchar(':');
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
