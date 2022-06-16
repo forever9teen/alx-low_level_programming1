ed9804fe192bcb54b66bf72d678fda16b827ab60
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-15 20:06:42 -0700

    .+10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * * *add - a function that adds two integers
+ * * returns result
+ * * *Returns: Always 0 (success)
+ * **/
+int add()
+{
+	int a;
+	int b;
+
+	n = a+b;
+	printf("%d + %d = %d\n", a,b,n);
+	return (0);
+}
+
