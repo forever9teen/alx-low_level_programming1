d6f9baf5a29c2a31e3c8b9f4502cb42098f6cbc9
forever9teen <opeloyeruwasiu19@gmail.com>
2022-06-16 03:30:21 -0700

    10. a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,10 +4,10 @@
  * * *add - a function that adds two integers
  * * @a: parameter1
  * @b: parameter2
- * * *Returns: returns a+b
+ * * *Return: returns a + b
  * **/
 int add(int a, int b)
 {
-	return (a+b);
+	return (a + b);
 }
 
